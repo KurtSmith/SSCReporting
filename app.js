15e3f62c9580537e02392a34a159af3dc1e82e58
kusmith <kurt.smith4@extendhealth.com>
2019-10-15 16:48:43 -0700

    test deploy again again again again again again

--- app.js
+++ app.js
@@ -10,5 +10,10 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('SSC Reporting!');
 });
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.send('SSC Reporting!');
+});
 console.log("*****************************Starting app********************************")
 app.listen(3000);
